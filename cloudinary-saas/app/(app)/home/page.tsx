45a1f35de47cb205fe6bf18e3aa22c82d5fcfecf
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-03-04 20:20:33 -0700

    update

--- cloudinary-saas/app/(app)/home/page.tsx
+++ cloudinary-saas/app/(app)/home/page.tsx
@@ -1,7 +1,9 @@
+// when folder are named in () it Route Groups which exlucdes folder name from the url path
+
 import React from "react";
 
 const Home = () => {
   return <div>Home</div>;
-}; 
+};
 
 export default Home;
--- cloudinary-saas/middleware.ts
+++ cloudinary-saas/middleware.ts
@@ -1,8 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { url } from "inspector";
+import { NextResponse } from "next/server";
 
-
-//All the routes that are public and can be accessed without login
 const isPublicRoute = createRouteMatcher([
     "/sign-in",
     "/sign-up",
@@ -14,35 +13,30 @@ const isPublicApiRoute = createRouteMatcher([
     "/api/videos"
 ])
 
-export default clerkMiddleware(async (auth, req) => {
-    const { userId } = await auth();
-    const currentUrl = new URL(req.url);
-    const isAccessingDashboard = currentUrl.pathname === "/home";
-    const isApiRequest = currentUrl.pathname.startsWith("/api");
 
-    // Add your middleware logic here
-    //if user is logged in and accessing a public route but not dashboard
+export default clerkMiddleware((auth, req) => {
+    const { userId } = auth();
+    const currentUrl = new URL(req.url);
+    const isAccessingDashboard = currentUrl.pathname === "/home"
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+
     if (userId && isPublicRoute(req) && !isAccessingDashboard) {
         return NextResponse.redirect(new URL("/home", req.url));
     }
 
-    // if not logged in
     if (!userId) {
-        //if user is not logged in and trying to access a protected route
-        if (isPublicApiRoute(req) && !isPublicApiRoute(req)) {
+        if (!isPublicApiRoute(req) && !isPublicRoute(req)) {
             return NextResponse.redirect(new URL("/sign-in", req.url))
         }
 
-
-        //if the request is for a protected API and the user is not logged in
-        if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+        if (isApiRequest && !isPublicApiRoute(req)) {
             return NextResponse.redirect(new URL("/sign-in", req.url))
         }
     }
+    return NextResponse.next()
+})
 
-    //In middleware it is very important because it needs to execute in between and transfer the request to wherever it needs to go
-    return NextResponse.next();
-});
 
 export const config = {
     matcher: [
@@ -50,3 +44,4 @@ export const config = {
         "/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)",
     ],
 }
+
