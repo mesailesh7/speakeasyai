f19fb11b384cd105fe85cdac67534d1d4b5bf598
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-03-12 01:04:20 -0600

    Update route.ts

--- nextjsbackend/src/app/movies/[id]/route.ts
+++ nextjsbackend/src/app/movies/[id]/route.ts
@@ -1,8 +1,25 @@
 import { movies } from '../db'
 
-export async function GET(_req: Request, { params }: Props) {
+export async function GET(_req: Request, { params }: { params: { id: string } }) {
     const { id } = await params;
 
+    // we are using + to convert it to the number or we can also use Number(id) to convert it to the number
+    const movie = movies.find(movie => movie.id === +id);
 
-    const movie = 
+    return movie ? new Response(JSON.stringify(movie)) : new Response("Movie not found", { status: 404 })
+}
+
+
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+    const { id } = await params;
+    const movieId = +id;
+
+    //check if the movie exists
+    const movie = movies.find(m => m.id === movieId)
+
+    if (!movie) {
+        return new Response(JSON.stringify({ error: "Movie not found :(" }), { status: 404 })
+    }
+
+
 }
