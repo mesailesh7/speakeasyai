749cc9b988411eb21cd8112e27f30bf31610ab6a
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-03-16 15:41:01 -0600

    SearchForm reset update
    making searchform use params

--- yc_directory/app/(root)/page.tsx
+++ yc_directory/app/(root)/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import SearchForm from "@/components/SearchForm";
 
-const Home = () => {
-  return <h1 className="heading">Home</h1>;
+// In every page in nextjs we have access to searchParams which gives us parameters from the url
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Enterpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
 };
 
 export default Home;
