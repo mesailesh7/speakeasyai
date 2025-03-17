9801c2c5e4ce7213c83582b02f95cebbff2ae3b6
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-03-17 11:04:30 -0600

    Sanity installed and configured

--- yc_directory/app/(root)/page.tsx
+++ yc_directory/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 // In every page in nextjs we have access to searchParams which gives us parameters from the url
 const Home = async ({
@@ -8,6 +9,20 @@ const Home = async ({
   searchParams: Promise<{ query?: string }>;
 }) => {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Sunny" },
+      description: "this is a description.",
+      image:
+        "https://images.unsplash.com/photo-1739897020176-a3123626d791?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Mountains",
+      title: "Beautiful Mountain",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -20,6 +35,20 @@ const Home = async ({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Result for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
