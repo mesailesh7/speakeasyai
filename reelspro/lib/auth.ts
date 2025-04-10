2c379a4c9cc527a65e4a9c7102d720a310c5f9b0
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-04-09 19:51:10 -0600

    auth.js update

---
+++ reelspro/lib/auth.ts
@@ -0,0 +1,89 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+
+            async authorize(credentials) {
+                /**
+                 * Checks if either email or password credentials are provided
+                 * Ensures that at least one of the authentication credentials is present
+                 */
+                if (credentials?.email || credentials?.password) {
+                    throw new Error("Missing Email or password");
+
+                }
+
+                try {
+
+                    await connectToDatabase()
+                    const user = await User.findOne({ email: credentials.email })
+
+                    if (!user) {
+                        throw new Error("No user found");
+                    }
+
+                    const isValid = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    )
+
+                    if (!isValid) {
+                        throw new Error("Invalid password");
+                    }
+
+                    return {
+                        id: user._id.toString(),
+                        email: user.email,
+
+                    }
+
+
+                } catch (error) {
+                    throw error;
+                }
+
+            }
+
+        }
+        )
+    ],
+
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token
+        },
+
+        async session({ session, token }) {
+
+            if (session.user) {
+                session.user.id = token.id as string;
+            }
+
+            return session;
+        },
+    },
+    pages: {
+        signIn: "/login",
+        error: "/login"
+    },
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60
+    },
+    secret: process.env.NEXTAUTH_SECRET
+
+};
