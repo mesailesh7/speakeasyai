dbbefd51f907c113dcbba6aabb6f36434fcfa96e
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-06-01 15:21:03 -0600

    user controller in progress
    1:14

--- ai-agent-full-stack/ai-ticket-assistant/controllers/user.js
+++ ai-agent-full-stack/ai-ticket-assistant/controllers/user.js
@@ -1,6 +1,30 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/user";
-import { innjest } from "../inngest/client";
+import { inngest } from "../inngest/client";
 
-export const signup = async (req, res) => {};
+export const signup = async (req, res) => {
+  const { email, password, skills = [] } = req.body;
+  try {
+    const hashed = bcrypt.hash(password, 10);
+    const user = await User.create({ email, password: hashed, skills });
+
+    //fire inngest event
+    await inngest.send({
+      name: "user/signup",
+      data: {
+        email,
+      },
+    });
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.json({ user, token });
+  } catch (error) {}
+};
